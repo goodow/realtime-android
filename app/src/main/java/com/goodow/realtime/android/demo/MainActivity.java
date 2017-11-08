87d3a971f6b6ffcb52789570a5a2698b43524248
Larry Tin <dev@goodow.com>
2017-11-08 14:25:54 +0800

    Implements Router goToUrl

--- app/src/main/java/com/goodow/realtime/android/demo/MainActivity.java
+++ app/src/main/java/com/goodow/realtime/android/demo/MainActivity.java
@@ -1,13 +1,17 @@
 package com.goodow.realtime.android.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.goodow.realtime.android.mvp.Router;
+import com.goodow.realtime.channel.AsyncResult;
 import com.goodow.realtime.channel.Bus;
+import com.goodow.realtime.channel.Handler;
 import com.goodow.realtime.channel.Message;
 import com.goodow.realtime.channel.impl.SimpleBus;
-import com.goodow.realtime.channel.AsyncResult;
 import com.goodow.realtime.channel.util.AsyncResultHandler;
-import com.goodow.realtime.channel.Handler;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +22,27 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(com.goodow.realtime.android.demo.R.layout.activity_main);
 
+    Router.init(getApplication());
     bus = new SimpleBus();
+
+    bus.subscribe("views/#", new Handler<Message>() {
+      @Override
+      public void handle(Message message) {
+        String url;
+        if (message.topic().equals("views")) {
+          boolean noData = message.payload() instanceof String;
+          if (noData) {
+            Router.getInstance().goToUrl((String) message.payload());
+            return;
+          }
+          url = (String) ((Map) message.payload()).get("url");
+        } else {
+          url = "test://" + message.topic();
+        }
+        Router.getInstance().withData(message.payload()).goToUrl(url);
+      }
+    });
+
     bus.subscribe("#", new Handler<Message>() {
       @Override
       public void handle(Message message) {
--- realtime-channel/src/main/java/com/goodow/realtime/android/mvp/RouteBuilder.java
+++ realtime-channel/src/main/java/com/goodow/realtime/android/mvp/RouteBuilder.java
@@ -3,10 +3,14 @@ package com.goodow.realtime.android.mvp;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -15,8 +19,47 @@ import java.util.UUID;
 
 public class RouteBuilder {
   static final String ACTIVITY_ID = RouteBuilder.class.getName();
-  private final Object data;
+  private Object data;
   private int flags = -1;         // Flags of route
+
+  /**
+   * Split query parameters
+   *
+   * @param rawUri raw uri
+   * @return map with params
+   */
+  private static Map<String, String> splitQueryParameters(Uri rawUri) {
+    String query = rawUri.getEncodedQuery();
+
+    if (query == null) {
+      return Collections.emptyMap();
+    }
+
+    Map<String, String> paramMap = new LinkedHashMap<>();
+    int start = 0;
+    do {
+      int next = query.indexOf('&', start);
+      int end = (next == -1) ? query.length() : next;
+
+      int separator = query.indexOf('=', start);
+      if (separator > end || separator == -1) {
+        separator = end;
+      }
+
+      String name = query.substring(start, separator);
+
+      if (!android.text.TextUtils.isEmpty(name)) {
+        String value = (separator == end ? "" : query.substring(separator + 1, end));
+        paramMap.put(Uri.decode(name), Uri.decode(value));
+      }
+
+      // Move start to end of name.
+      start = end + 1;
+    } while (start < query.length());
+
+    return Collections.unmodifiableMap(paramMap);
+  }
+
 
   public RouteBuilder(Object data) {
     this.data = data;
@@ -43,7 +86,53 @@ public class RouteBuilder {
   }
 
   public void goToUrl(String url) {
+    Uri uri = Uri.parse(url);
+    if (null == uri || uri.toString().length() == 0) {
+      throw new RuntimeException("url Parameter invalid!");
+    }
+    Map<String, String> parameters = this.splitQueryParameters(uri);
+    if (parameters.size() > 0) {
+      if (this.data == null) {
+        this.data = parameters;
+      } else if (this.data instanceof Map) {
+        ((Map) this.data).putAll(parameters);
+      }
+    }
+    if (this.data instanceof Map) {
+      parameters = (Map<String, String>) this.data;
+    }
 
+    if (parameters.containsKey("viewOpt.flags")) {
+      this.flags = Integer.parseInt(parameters.get("viewOpt.flags"));
+    }
+
+    String className = uri.getLastPathSegment();
+    if (!className.endsWith(Activity.class.getSimpleName())) {
+      className = className + Activity.class.getSimpleName();
+    }
+    String rootPackageName = Router.getInstance().context.getPackageName();
+    String packageName;
+    if (parameters.containsKey("viewOpt.package")) {
+      packageName = parameters.get("viewOpt.package");
+      if (packageName.startsWith(".")) {
+        packageName = rootPackageName + packageName;
+      }
+    } else if (!className.startsWith(rootPackageName)) {
+      packageName = rootPackageName;
+    } else {
+      packageName = "";
+    }
+    className = packageName + (className.startsWith(".") ? "" : ".") + className;
+    Class<?> aClass;
+    try {
+      aClass = Class.forName(className);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+    if (!Activity.class.isAssignableFrom(aClass)) {
+      throw new RuntimeException("className Parameter invalid!");
+    }
+    this.goToClass((Class<? extends Activity>) aClass);
   }
 
   /**
